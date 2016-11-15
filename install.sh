45292b872a4e331beb01a7c40ac825f294029fda
Ioannis Giakoumidis <ioannis@byng.co>
2016-11-15 16:43:06 +0000

    Case:Correct variable name

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRA_DIR="$HOME/.extra"
-$UNAME="Darwin"
+UNAME="Darwin"
 
 # Update dotfiles itself first
 
